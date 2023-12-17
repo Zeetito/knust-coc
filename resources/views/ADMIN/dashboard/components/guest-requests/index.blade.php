e1a95ca5bdf88e360a43417c85b53d1199aaa146
Zeetito <agyareernest44@gmail.com>
2023-12-17 14:52:09 +0000

    Phase 18 in progress - another correction - Assign/index page

--- resources/views/ADMIN/dashboard/components/guest-requests/index.blade.php
+++ resources/views/ADMIN/dashboard/components/guest-requests/index.blade.php
@@ -45,14 +45,12 @@
                         <div class="card-body">
                             
                         
-                            <span class=" float-right btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('assign_guest_request',['guest_request'=>$request])}}" >Assign To</span>
 
                             <div class=" mb-0">{{$request->created_at->diffInDays(now())}} Days Ago</div>
                             <small class="text-uppercase font-weight-bold">{{$request->guest()->fullname." - ".$request->guest()->status." (".$request->method." ".$request->type.")" }}</small>
                             <div class="font-weight-bold">Contact: {{$request->guest()->contact}}</div>
                             @if($request->is_assigned() == false)
                             <span class=" float-right btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('assign_guest_request',['guest_request'=>$request])}}" >Assign To</span>
-                            {{-- <span class="btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('edit_guest_request',['guest_request'=>$request])}}"   >Handle</span> --}}
                             @else
                             <span class=" float-right btn fa fa-check"  >Assigned - {{$request->assigned_user()->fullname()}}</span>
                             @endif
