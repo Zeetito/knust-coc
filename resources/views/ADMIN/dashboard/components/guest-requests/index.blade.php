c0951855a2b13547fe7d13802d61279cba0e387d
Zeetito <agyareernest44@gmail.com>
2023-12-30 09:03:15 +0000

    Fixed Guest request index page

--- resources/views/ADMIN/dashboard/components/guest-requests/index.blade.php
+++ resources/views/ADMIN/dashboard/components/guest-requests/index.blade.php
@@ -43,9 +43,9 @@
             @foreach(App\Models\Guest::guest_requests()->get() as $request)
             
                 
-                <div class="col-sm-3 col-md-2 mt-3">
+                <div class="col-sm-3 col-md-3 mt-3">
                     {{-- If User is a Fresher --}}
-                    @if($request->guest()->status == 'fresher')
+                    {{-- @if($request->guest()->status == 'fresher') --}}
 
                     @if($request->is_assigned() == true)
                         <div class="card text-white bg-success" >
@@ -54,10 +54,12 @@
                     @endif
                         <div class="card-body">
                             
-                        
+                            
 
                             <div class=" mb-0">{{$request->created_at->diffInDays(now())}} Days Ago</div>
-                            <small class="text-uppercase font-weight-bold">{{$request->guest()->fullname." - ".$request->guest()->status." (".$request->method." ".$request->type.")" }}</small>
+                            <small class="text-uppercase font-weight-bold">{{$request->guest()->fullname}}</small><br>
+                            <small class="text-uppercase font-weight-bold">Status:{{$request->guest()->status." (".$request->method." ".$request->type.")" }}</small><br>
+                            <small class="text-uppercase font-weight-bold">Request:({{$request->method." ".$request->type}})</small><br>
                             <div class="font-weight-bold">Contact: {{$request->guest()->contact}}</div>
                             @if($request->is_assigned() == false)
                             <span class=" float-right btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('assign_guest_request',['guest_request'=>$request])}}" >Assign To</span>
@@ -68,56 +70,7 @@
                          
                         </div>
                     </div>
-
-                    {{-- IF User is A continous Student --}}
-                    @elseif($request->guest()->status == 'member')
-                        @if($request->is_assigned() == true)
-                        <div class="card text-white bg-success" >
-                        @else
-                        <div class="card text-white bg-primary" >
-                        @endif
-
-                        <div class="card-body">
-                        
-                            
-                            <div class=" mb-0">{{$request->created_at->diffInDays(now())}} Days Ago</div>
-                            <small class="text-uppercase font-weight-bold">{{$request->guest()->fullname." - ".$request->guest()->status." (".$request->method." ".$request->type.")" }}</small>
-                            <div class="font-weight-bold">Contact: {{$request->guest()->contact}}</div>
-
-                            @if($request->is_assigned() == false)
-                            <span class=" float-right btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('assign_guest_request',['guest_request'=>$request])}}" >Assign To</span>
-                            {{-- <span class="btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('edit_guest_request',['guest_request'=>$request])}}"   >Handle</span> --}}
-                            @else
-                            <span class=" float-right btn fa fa-check"  >Assigned - {{$request->assigned_user()->fullname()}}</span>
-                            @endif
-                         
-                        </div>
-                    </div>
-
-                    @elseif($request->guest()->status == 'alumni' || $request->guest()->status == 'alumini')
-                        @if($request->is_assigned() == true)
-                        <div class="card text-white bg-success" >
-                        @else
-                        <div class="card text-white bg-primary" >
-                        @endif
-
-                        <div class="card-body">
-                        
-                            
-                            <div class=" mb-0">{{$request->created_at->diffInDays(now())}} Days Ago</div>
-                            <small class="text-uppercase font-weight-bold">{{$request->guest()->fullname." - ".$request->guest()->status." (".$request->method." ".$request->type.")" }}</small>
-                            <div class="font-weight-bold">Contact: {{$request->guest()->contact}}</div>
-
-                            @if($request->is_assigned() == false)
-                            <span class=" float-right btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('assign_guest_request',['guest_request'=>$request])}}" >Assign To</span>
-                            {{-- <span class="btn fa fa-pencil" data-toggle="modal" data-target="#myModal" data-url="{{route('edit_guest_request',['guest_request'=>$request])}}"   >Handle</span> --}}
-                            @else
-                            <span class=" float-right btn fa fa-check"  >Assigned - {{$request->assigned_user()->fullname()}}</span>
-                            @endif
-                        
-                        </div>
-                    </div>
-                    @endif
+                    {{-- @endif --}}
 
 
 
